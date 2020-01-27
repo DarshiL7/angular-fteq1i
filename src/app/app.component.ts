9b145ec95f1906d336cb9371196da4ae1c6c51b9
DarshiL7 <darshilkansara8891@gmail.com>
2020-01-27 11:32:15 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit  {
 
 
 //  }
-//  dataPass(){
-//    console.log(this.userFormGroup.value);
-//  } 
+ dataPass(){
+   console.log(this.userFormGroup.value);
+ } 
 }
