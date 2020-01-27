75b70657233857d512c265e49169c864fd231a31
DarshiL7 <darshilkansara8891@gmail.com>
2020-01-27 10:47:57 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+//import {User} from './user.model.ts';
+import { FormBuilder,FormGroup,Validators } from "@angular/forms"
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
+  //user:User=new User();
+  // userFormGroup:FormGroup;
+
+  // constructor(private formBuilder:FormBuilder){
+
+  // }
  ngOnInit(){
-   firstName:''
-   lastName:''
-   email:''
-   password:''
-   mobileNo:''
-   day:''
-   month:''
-   year:''
-   gender:''
+  //  this.userFormGroup=this.formBuilder.group({
+
+  //    firstName:['',Validators.required],
+  //  lastName:['',Validators.required],
+  //  email:['',Validators.required],
+  //  password:['',Validators.required],
+  //  mobileNo:['',Validators.required],
+  //  day:['',Validators.required],
+  //  month:['',Validators.required],
+  //  year:['',Validators.required],
+  //  gender:['',Validators.required]
+  //  })
+   
 
 
  }
  dataPass(){
-   console.log();
+   console.log(this.userFormGroup.value);
  } 
 }
---
+++ src/app/user.model.ts
@@ -0,0 +1,14 @@
+export class User
+{
+  firstName:string;
+  lastName:string;
+  email:string;
+  password:string;
+  mobileNo:number;
+  month:string;
+  day:string;
+  year:string;
+  gender:string;
+
+
+}
