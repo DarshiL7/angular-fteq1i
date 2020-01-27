5a51aa3cd9c2da37e27d843cc0fc2640f0fc3852
DarshiL7 <darshilkansara8891@gmail.com>
2020-01-27 11:07:04 +0530

    1 file modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,29 @@ import { FormBuilder,FormGroup,Validators } from "@angular/forms"
 })
 export class AppComponent implements OnInit  {
   //user:User=new User();
-  // userFormGroup:FormGroup;
+//   userFormGroup:FormGroup;
 
-  // constructor(private formBuilder:FormBuilder){
+//   constructor(private formBuilder:FormBuilder){
 
-  // }
- ngOnInit(){
-  //  this.userFormGroup=this.formBuilder.group({
+//   }
+//  ngOnInit(){
+//    this.userFormGroup=this.formBuilder.group({
 
-  //    firstName:['',Validators.required],
-  //  lastName:['',Validators.required],
-  //  email:['',Validators.required],
-  //  password:['',Validators.required],
-  //  mobileNo:['',Validators.required],
-  //  day:['',Validators.required],
-  //  month:['',Validators.required],
-  //  year:['',Validators.required],
-  //  gender:['',Validators.required]
-  //  })
+//     firstName:['',Validators.required],
+//     lastName:['',Validators.required],
+//     email:['',Validators.required],
+//     password:['',Validators.required],
+//     mobileNo:['',Validators.required],
+//     day:['',Validators.required],
+//     month:['',Validators.required],
+//     year:['',Validators.required],
+//     gender:['',Validators.required]
+//    })
    
 
 
- }
- dataPass(){
-   console.log(this.userFormGroup.value);
- } 
+//  }
+//  dataPass(){
+//    console.log(this.userFormGroup.value);
+//  } 
 }
